c4793190a72ad2dc27a96cddaf86e225bbf3a271
Hemanth Lakshman Raju <hlmayhem@gmail.com>
2023-07-27 15:05:18 -0700

    initial commit

---
+++ tasks/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.shortcuts import render
+from .models import Task
+
+def task_list(request):
+    tasks = Task.objects.all()
+    print(tasks)
+    return render(request, 'tasks/task_list.html', {'tasks':tasks})
